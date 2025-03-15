d73334d1b74d1adf10fd646b573aec1ff35c607d
sndp bag <sndpbagg@gmail.com>
2025-03-15 12:20:15 +0530

    added home page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,11 +3,12 @@ import {
     createBrowserRouter,
    
   } from "react-router-dom";
+import Home from "../pages/Home";
  
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Home></Home>,
     },
   ]);
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='container min-h-screen mx-auto bg-slate-100'>
+            
+        </div>
+    );
+};
+
+export default Home;
