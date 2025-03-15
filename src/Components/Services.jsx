bf085afcf2ed24e69883f56a898f2e3f98f8e49a
sndp bag <sndpbagg@gmail.com>
2025-03-15 19:56:53 +0530

    feat: update services component

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -40,25 +40,7 @@ const Services = () => {
             
         </div>
 
-    //     <div className="w-11/12 mx-auto mt-[140px]">
-    // <h2 className="text-[#179800] font-sora text-4xl font-semibold text-center md:text-left">
-    //     Services
-    // </h2>
-    // <div className="w-11/12 bg-amber-200 grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-3 p-6 rounded-lg">
-    //     {/* Service 1 */}
-    //     <div className="bg-[#EFEBE3] flex flex-col items-center justify-center p-6 rounded-lg shadow-lg">
-    //         <div className="w-40 h-40 rounded-full bg-white flex justify-center items-center shadow-md">
-    //             <img src={service1} alt="24/7 Service" className="w-24 h-24" />
-    //         </div>
-    //         <h2 className="text-center font-semibold text-lg mt-4">24/7 Services</h2>
-    //         <p className="text-center text-sm text-gray-700 mt-2">
-    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
-    //         </p>
-    //     </div>
-
-     
-//     </div>
-// </div>
+ 
 
     );
 };
